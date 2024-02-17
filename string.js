183798fc4e961bb80e32ec739b278f05d03f9c5f
heyosseus <ratiruxadzee@gmail.com>
2024-02-17 19:04:06 +0400

    add string method examples

--- string.js
+++ string.js
@@ -1,25 +1,41 @@
-let text = 'Hello from math/index.js!';
-let text2 = 'test';
+let text = 'Hello from Edutech!';
 
-// console.log(text.replace('math/index.js', 'math'));
-// console.log((text + ' ').repeat(2));
-// console.log(text.includes('math'));
-// console.log(text.startsWith('Hello'));
+let text2 = 'TEST';
+
+let result = `${text} ${text2}`;
+// console.log(result);
+// console.log(text.length);
+
+// console.log(text.replace('Edutech', 'World'));
+// console.log(text.repeat(2));
+// console.log(text.includes('from'));
+// console.log(text.startsWith('H'));
 // console.log(text.endsWith('!'));
-// console.log(text.match(/math/g));
+// console.log(text.match(/from/g));
 // console.log(text.concat(' ', text2));
 // console.log(text.trim());
-// console.log(text.search('math'));
+// console.log(text.search('H'));
 // console.log(text.split(' '));
-// console.log(text.slice(2, 4));
-console.log(text.substring(1, 4));
+// console.log(text.slice(-4, -2));
+// console.log(text.charAt(0));
+// console.log(text.lastIndexOf('l'));
+// console.log(text.toUpperCase());
+// console.log(text2.toLowerCase());
+// ====================================
+// console.log(text.substring(2));
 
-function makeLowerCase(string) {
-  return string.toLowerCase();
-}
-// console.log(makeLowerCase(text));
+// function makeLowerCase(string) {
+//   return string.toLowerCase();
+// }
+// // console.log(makeLowerCase(text));
 
-function returnFirstIndex(string) {
-  return string[0];
-}
+// function returnFirstIndex(string) {
+//   return string[0];
+// }
 // console.log(returnFirstIndex(text));
+
+function returnStringLength(str) {
+  console.log(str.length)
+}
+
+returnStringLength(text);
