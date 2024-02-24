ce6e22c4d2bf4eab6949552f2a2674714b43f43b
heyosseus <ratiruxadzee@gmail.com>
2024-02-24 16:35:41 +0400

    add math functions

---
+++ math.js
@@ -0,0 +1,12 @@
+// console.log(Math.PI);
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(7.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.abs(-4.7));
+// console.log(Math.min(0, 150, 30, 20, -8, -200));
+// console.log(Math.max(0, 150, 30, 20, -8, -200));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10));
+// console.log(Math.pow(8, 2));
+// console.log(Math.trunc(4.787897392739173123));
